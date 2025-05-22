1ee06f3fa3ecd01567af55dc6d738fdd0d40cb1c
Pavle Vesovic <119006421+r1spekt1@users.noreply.github.com>
2025-05-22 09:43:38 +0200

    Update PasswordGen.py

--- PasswordGen.py
+++ PasswordGen.py
@@ -9,7 +9,7 @@ letters = [
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+']
 
-print("Welcome to the PyPassword Generator!")
+print("Welcome to the Password Generator!")
 nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input("How many symbols would you like?\n"))
 nr_numbers = int(input("How many numbers would you like?\n"))
